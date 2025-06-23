ebfbc1892883c92f6a79efb0f273b5fa6dab69e3
Haider Mukhtar <haidermukhtarch405@gmail.com>
2025-06-23 21:42:23 +0500

    Fixed Responsive Bug

--- src/components/experience-section.tsx
+++ src/components/experience-section.tsx
@@ -55,6 +55,7 @@ const ExperienceSection = () => {
               <motion.div
                 key={index}
                 // initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
+                initial={{ opacity: 0, y: 0 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.6, delay: index * 0.1 }}
                 viewport={{ once: true }}
